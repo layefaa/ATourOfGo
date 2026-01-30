514644ef510276d2b47d03b26ff02119e03ef555
Martin Luther Obahor <kingm4159@gmail.com>
2026-01-30 06:24:06 +0100

    Add example illustrating advanced `defer` usage and file operations

---
+++ basic/understanding-defer-in-depth.go
@@ -0,0 +1,51 @@
+//go:build defer
+// +build defer
+
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+func CopyFile(dstName, srcName string) (written int64, err error) {
+	src, err := os.Open(srcName)
+	if err != nil {
+		return
+	}
+	defer src.Close()
+
+	dst, err := os.Create(dstName)
+	if err != nil {
+		return
+	}
+	defer dst.Close()
+
+	return io.Copy(dst, src)
+}
+
+func a() {
+	i := 0
+	defer fmt.Println(i)
+	i++
+	return
+}
+
+func b() {
+	for i := 0; i < 4; i++ {
+		defer fmt.Println(i)
+	}
+}
+
+func cc() (i int) {
+	defer func() { i++ }()
+	return i
+
+}
+
+func main() {
+	//a()
+	//b()
+	cc()
+}
